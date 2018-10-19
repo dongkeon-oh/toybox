e7d42d9f691a3701b901c14a821610d8f1248d97
dongkeon-oh <dongkeon.oh@gmail.com>
2018-10-20 01:24:35 +0900

    페이징 개발중

--- toybox/src/main/webapp/resources/js/common_js/list_common.js
+++ toybox/src/main/webapp/resources/js/common_js/list_common.js
@@ -1,15 +1,19 @@
 $(function() {
-	list_common(1, "");
+	list_common(1);
 });
 
-function list_common(pageNo, orderType){
+function list_common(page_no){
+	list_common(page_no, $("#idx").val());
+}
+
+function list_common(page_no, order_type){
 	var target = $("#target").val();
     var	keyword = $("#keyword").val();
-    var	end_idx = $("#idx").val()*1*pageNo;
-    var	start_idx = end_idx-pageNo+1;
+    var	end_idx = $("#idx").val()*1*page_no;
+    var	start_idx = end_idx-(page_no*1)+1;
     
-    if(orderType == undefined || orderType == ""){
-    	orderType = "COM_ID";
+    if(order_type == undefined || order_type == ""){
+    	order_type = "COM_ID";
     }
     
     var param = {
@@ -17,7 +21,7 @@ function list_common(pageNo, orderType){
     	"keyword" : keyword,
     	"start_idx" : start_idx,
     	"end_idx" : end_idx,
-    	"orderType" : orderType
+    	"order_type" : order_type
     }
     //var paramJson = JSON.stringify(param);
 	
@@ -25,23 +29,45 @@ function list_common(pageNo, orderType){
         method:"POST",
         url:"ajax_list_common",
 //        datatype:"json",
-        data:{
-        	"target" : target,
-        	"keyword" : keyword
-        },
+        data:param,
 //        contentType:"application/json;charset=UTF-8",
         async:false,
         success:function(response){
         	var result = response;
         	var opt = "";
+        	var cnt = 0;
+        	
         	$("table").html("");
         	$.each(result, function(index, item){
+        		cnt++;
         		opt = opt + "<tr><td>"+(index+1)+"</td><td>"+item.com_id+"</td><td>"+item.com_name+"</td><td>"+item.com_category1+"</td><td>"+item.com_category2+"</td><td>"+item.com_category3+"</td></tr>";
         	});
         	$("table").append(opt);
+        	
+        	set_pagination(cnt, order_type, page_no)
         },
         error:function(request,status,error){
             console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
         }
     });
+}
+
+function set_pagination(cnt, order_type, page_no){
+	var total_page = 0;
+	if(cnt%order_type > 0){
+		total_page = 1;
+	}
+	total_page = cnt/order_type + total_page;
+
+	var page = "";
+	var append = "";
+	for(var i = 1; total_page >= i; i++){
+		if(i == page_no){
+			append = '<li class="disabled"><span onclick="list_common('+i+')">'+i+'</span></li>';
+		}else{
+			append = '<li class="active"><span onclick="list_common('+i+')">'+i+'</span></li>';
+		}
+		page = page + append;
+	}
+	$("#pagination_area").append(page);
 }
