d17dc35081ddf98bce0b8469a401ea06c8a7b2b9
dongkeon-oh <dongkeon.oh@gmail.com>
2018-11-24 14:23:08 +0900

    공통코드 생성기능 구현중

--- toybox/src/main/java/com/dongkeonoh/toybox/vo/CommonVo.java
+++ toybox/src/main/java/com/dongkeonoh/toybox/vo/CommonVo.java
@@ -1,12 +1,24 @@
 package com.dongkeonoh.toybox.vo;
 
 public class CommonVo {
+	// 공통코드 그룹
 	private String cgr_group;
 	private String cgr_group_name;
     private String cgr_note;
     private String cgr_useyn;
     private String cnt;
-    
+	// 공통코드
+	private String ccd_seq;      
+	private String ccd_code;     
+	private String ccd_group;   
+	private String ccd_codename; 
+	private String ccd_detail1;  
+	private String ccd_detail2;  
+	private String ccd_detail3;  
+	private String ccd_note;     
+	private String ccd_order;    
+	private String ccd_useyn;
+	
 	public String getCgr_group() {
 		return cgr_group;
 	}
@@ -36,5 +48,65 @@ public class CommonVo {
 	}
 	public void setCnt(String cnt) {
 		this.cnt = cnt;
+	}
+	public String getCcd_seq() {
+		return ccd_seq;
+	}
+	public void setCcd_seq(String ccd_seq) {
+		this.ccd_seq = ccd_seq;
+	}
+	public String getCcd_code() {
+		return ccd_code;
+	}
+	public void setCcd_code(String ccd_code) {
+		this.ccd_code = ccd_code;
+	}
+	public String getCcd_group() {
+		return ccd_group;
+	}
+	public void setCcd_group(String ccd_group) {
+		this.ccd_group = ccd_group;
+	}
+	public String getCcd_codename() {
+		return ccd_codename;
+	}
+	public void setCcd_codename(String ccd_codename) {
+		this.ccd_codename = ccd_codename;
+	}
+	public String getCcd_detail1() {
+		return ccd_detail1;
+	}
+	public void setCcd_detail1(String ccd_detail1) {
+		this.ccd_detail1 = ccd_detail1;
+	}
+	public String getCcd_detail2() {
+		return ccd_detail2;
+	}
+	public void setCcd_detail2(String ccd_detail2) {
+		this.ccd_detail2 = ccd_detail2;
+	}
+	public String getCcd_detail3() {
+		return ccd_detail3;
+	}
+	public void setCcd_detail3(String ccd_detail3) {
+		this.ccd_detail3 = ccd_detail3;
+	}
+	public String getCcd_note() {
+		return ccd_note;
+	}
+	public void setCcd_note(String ccd_note) {
+		this.ccd_note = ccd_note;
+	}
+	public String getCcd_order() {
+		return ccd_order;
+	}
+	public void setCcd_order(String ccd_order) {
+		this.ccd_order = ccd_order;
+	}
+	public String getCcd_useyn() {
+		return ccd_useyn;
+	}
+	public void setCcd_useyn(String ccd_useyn) {
+		this.ccd_useyn = ccd_useyn;
 	}
 }
