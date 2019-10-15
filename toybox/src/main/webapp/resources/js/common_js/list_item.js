16ab78d85a38f2769e145d41df384b13593b21d9
dongkeon-oh <dongkeon.oh@gmail.com>
2019-10-15 19:32:32 +0900

    아이템 등록 마무리중

--- toybox/src/main/webapp/resources/js/common_js/list_item.js
+++ toybox/src/main/webapp/resources/js/common_js/list_item.js
@@ -18,7 +18,17 @@ $(function() {
 	});
 	
 	$("#put_item").on("click",function(){
+		item_init();
 		$("#item_modal_title").html("아이템 추가");
+		$("#btn_modify").html("추가");
+	});
+	
+	$(document).on("click", ".detail_item", function(){
+		item_init();
+		var item_id = $(this).attr("id");
+		detail_item(item_id);
+		$("#item_modal_title").html("아이템 수정");
+		$("#btn_modify").html("수정");
 	});
 	
 	$(".item_sub_option").on("click",function(){
@@ -49,15 +59,46 @@ $(function() {
 			search_value("itm_owner", "_name");
 		}
 	});	
+	
+	$("#btn_modify").on("click",function(){
+		var item_id = $("#itm_id").val();
+		if(item_id == undefined || item_id == ""){
+			put_item();
+		}else{
+			update_item(item_id);
+		}
+	});
 });
 
 function put_item() {
 	var item_name = $("#itm_name").val();
 	var item_type = $("#itm_type").val();
 	var item_owner = $("#itm_owner").val();
+	var item_image = $("#itm_image").val();
 	var item_mainitem = $("#itm_mainitem").val();
+	var item_useyn = $("#itm_useyn").val();
 	var item_note = $("#itm_note").val();
 
+	if(item_name.replace(/(\s)/g,"") == "" || item_name == undefined){
+		alert("아이템 이름을 확인하십시오.");
+		$("#itm_name").focus();
+	}else if(get_byte_calc(item_name) > 1000){
+		alert("아이템 이름이 너무 깁니다. 100byte 미만으로 작성해주시기 바랍니다.");
+		$("#itm_name").focus();
+	}else if(item_type == ""){
+		alert("아이템 상태를 입력해 주시기 바랍니다.");
+		$("#sub_itm_type").focus();
+	}else if(item_owner == ""){
+		alert("소유자를 입력해 주시기 바랍니다.");
+		$("#sub_itm_owner").focus();
+	}else if(item_useyn == ""){
+		alert("아이템 상태를 입력해 주시기 바랍니다.");
+		$("#sub_itm_useyn").focus();
+	}else if(get_byte_calc(item_note) > 1000){
+		alert("설명이 너무 깁니다. 1000byte 미만으로 작성해주시기 바랍니다.");
+		$("#itm_note").focus();
+	}	
+		
 	$.ajax({
 		method : "POST",
 		url : "ajax_put_item",
@@ -65,13 +106,25 @@ function put_item() {
 			"itm_name" : item_name,
 			"itm_type" : item_type,
 			"itm_owner" : item_owner,
+			"itm_image" : item_image,
 			"itm_mainitem" : item_mainitem,
-			"itm_note" : item_note
+			"itm_useyn" : item_useyn,
+			"itm_note" : item_note,
+			
+			"cdt_condition" : "rentable",
+//			"cdt_item" : ???,
+			"cdt_user" : item_owner,
+			"cdt_location" : item_owner,
+			"cdt_return" : item_owner,
+			"cdt_note" : "신규 생성",
+			"cdt_fromdate" : "2100/12/31",
+			"cdt_todate" : "1989/06/07"
 		},
 		async : false,
 		success : function(response) {
 			if(response > 0){
 				alert("["+item_name+"] 아이템이 추가되었습니다.");
+				list_item(pagination_index, pagination_cnt, keyword_data);
 			}else{
 				alert("["+item_name+"] 아이템 추가에 실패했습니다.");
 			}
@@ -80,6 +133,20 @@ function put_item() {
 			console.log("code:" + request.status + "\n" + "message:"+ request.responseText + "\n" + "error:" + error);
 		}
 	});
+}
+
+function item_init(){
+	$("#itm_id").val("");
+	$("#itm_name").val("");
+	$("#itm_type").val("");
+	$("#itm_type_code").val("");
+	$("#itm_owner").val("");
+	$("#itm_owner_name").val("");
+	$("#itm_mainitem").val("");
+	$("#itm_mainitem").val("");
+	$("#itm_useyn").val("");
+	$("#itm_useyn_code").val("");
+	$("#itm_note").val("");
 }
 
 function get_common_code_search(c_group) {
@@ -186,14 +253,18 @@ function search_init(init_type){
 }
 
 function search_value(value_type, value_tail){
-	if($("#sub_option").val() == "_NODATA_"){
+	var s_option = $("#sub_option").val();
+	if(s_option == "_NODATA_" && value_type != "itm_mainitem"){
 		alert($(".label_text").text()+"을(를) 확인해주십시오.");
-		return;
-	}
-	$("#"+value_type).val($("#sub_option").val());
-	$("#"+value_type+value_tail).val($("#sub_option option:selected").text());
-	
-	$('#sub_modal').modal("hide");
+	}else if(value_type == "itm_mainitem"){
+		$("#"+value_type).val("");
+		$("#"+value_type+value_tail).val("");
+		$('#sub_modal').modal("hide");
+	}else{
+		$("#"+value_type).val(s_option);
+		$("#"+value_type+value_tail).val($("#sub_option option:selected").text());
+		$('#sub_modal').modal("hide");
+	}	
 }
 
 function list_item(page_no, page_cnt, keyword) {
@@ -203,7 +274,7 @@ function list_item(page_no, page_cnt, keyword) {
 
 	$.ajax({
 		method : "POST",
-		url : "ajax_admin_item",
+		url : "ajax_list_item_admin",
 		data : {
 			"keyword" : keyword,
 			"start_idx" : start_idx,
@@ -216,35 +287,24 @@ function list_item(page_no, page_cnt, keyword) {
 			var page_total = 0;
 
 			$("tbody").html("");
-			$.each(
-				result,
-				function(index, item) {
+			$.each(result, function(index, item) {
 					page_total = item.cnt;
-
-					var item_conditon = "사용중";
-					var table_row_type = "";
-					if (item.itm_useyn == "M") {
-						item_conditon = "분실";
-						table_row_type = "bg-danger";
-					}else if (item.itm_useyn == "D"){
-						item_conditon = "폐기";
-						table_row_type = "bg-danger";
-					}
-
-					opt = opt + "<tr class='" + table_row_type + "' id='" + item.itm_id +"'>";
+					var table_row_type = "";	// 아이템 상태 추가시 사용
+					
+					opt = opt + "<tr class='" + table_row_type + "'>";
 					opt = opt + "	<td>" + (start_idx + index) + "</td>";
 					opt = opt + "	<td>" + item.itm_name + "</td>";
-					opt = opt + "	<td>" + item_conditon + "</td>";
-					opt = opt + "	<td>" + item.itm_owner + "</td>";
+					opt = opt + "	<td>" + item.itm_type_code + "</td>";
+					opt = opt + "	<td>" + item.itm_owner_name + "</td>";
 					opt = opt + "	<td>";
-					opt = opt + "		<button type='button' class='btn btn-success btn-sm' data-toggle='modal' data-target='#item_modal' onClick='item_info(\"" + item.itm_id + "\")' >상세보기</button>";
+					opt = opt + "		<button type='button' class='btn btn-success btn-sm detail_item' data-toggle='modal' data-target='#item_modal' id='" + item.itm_id + "' >상세보기</button>";
 					opt = opt + "	</td>";
 					opt = opt + "</tr>";
 				}
 			);
 			$("tbody").html(opt);
 
-			set_pagination(page_cnt, page_total, page_no, keyword, "");
+			set_pagination(pagination_cnt, page_total, pagination_index, keyword, "");
 		},
 		error : function(request, status, error) {
 			console.log("code:" + request.status + "\n" + "message:"+ request.responseText + "\n" + "error:" + error);
@@ -264,7 +324,7 @@ function search_enter(type) {
 	}
 }
 
-function item_info(itm_id) {
+function detail_item(itm_id) {
 	$.ajax({
 		method : "POST",
 		url : "ajax_detail_item",
@@ -274,79 +334,79 @@ function item_info(itm_id) {
 		async : false,
 		success : function(response) {
 			var result = response;
-			
-			var useyn = "사용중";
-			if(result.itm_useyn == "M"){
-				useyn = "분실";
-			}else if(result.itm_useyn == "D"){
-				useyn = "폐기";
-			}
 
+			$("#itm_id").val(result.itm_id);
 			$("#itm_name").val(result.itm_name);
 			$("#itm_type").val(result.itm_type);
+			$("#itm_type_code").val(result.itm_type_code);
 			$("#itm_owner").val(result.itm_owner);
+			$("#itm_owner_name").val(result.itm_owner_name);
 			$("#itm_mainitem").val(result.itm_mainitem);
-			$("#itm_useyn").val(useyn);
+			$("#itm_mainitem_name").val(result.itm_mainitem_name);
+			$("#itm_useyn").val(result.itm_useyn);
+			$("#itm_useyn_code").val(result.itm_useyn_code);
 			$("#itm_note").val(result.itm_note);
-
-			$("#btn_modify").html("수정");
-			$("#btn_modify").val("M");
 		},
 		error : function(request, status, error) {
 			alert("아이템 조회에 실패하였습니다.");
-			console.log("code:" + request.status + "\n" + "message:"
-					+ request.responseText + "\n" + "error:" + error);
+			console.log("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
 		}
 	});
 }
 
-//item_sub_option
-function sub_option(type, keyword) {
-	var sub_url = "ajax_item_type";
-	if(type == 'itm_owner'){
-		sub_url = "ajax_item_owner"
-	}else if(type == 'btn_itm_mainitem'){
-		sub_url = "ajax_mainitem_list"
-	}else if(type == 'itm_useyn'){
-		sub_url = "ajax_item_useyn"
-	}
-	
+function update_item(item_id) {
+	var item_name = $("#itm_name").val();
+	var item_type = $("#itm_type").val();
+	var item_owner = $("#itm_owner").val();
+	var item_image = $("#itm_image").val();
+	var item_mainitem = $("#itm_mainitem").val();
+	var item_useyn = $("#itm_useyn").val();
+	var item_note = $("#itm_note").val();
+
+	if(item_name.replace(/(\s)/g,"") == "" || item_name == undefined){
+		alert("아이템 이름을 확인하십시오.");
+		$("#itm_name").focus();
+	}else if(get_byte_calc(item_name) > 1000){
+		alert("아이템 이름이 너무 깁니다. 100byte 미만으로 작성해주시기 바랍니다.");
+		$("#itm_name").focus();
+	}else if(item_type == ""){
+		alert("아이템 상태를 입력해 주시기 바랍니다.");
+		$("#sub_itm_type").focus();
+	}else if(item_owner == ""){
+		alert("소유자를 입력해 주시기 바랍니다.");
+		$("#sub_itm_owner").focus();
+	}else if(item_useyn == ""){
+		alert("아이템 상태를 입력해 주시기 바랍니다.");
+		$("#sub_itm_useyn").focus();
+	}else if(get_byte_calc(item_note) > 1000){
+		alert("설명이 너무 깁니다. 1000byte 미만으로 작성해주시기 바랍니다.");
+		$("#itm_note").focus();
+	}	
+		
 	$.ajax({
 		method : "POST",
-		url : sub_url,
+		url : "ajax_update_item",
 		data : {
-			"keyword" : keyword
+			"itm_id" : item_id,
+			"itm_name" : item_name,
+			"itm_type" : item_type,
+			"itm_owner" : item_owner,
+			"itm_image" : item_image,
+			"itm_mainitem" : item_mainitem,
+			"itm_note" : item_note,
+			"itm_useyn" : item_useyn
 		},
 		async : false,
 		success : function(response) {
-			$("#sub_option").html("");
-			
-			var result = response;
-			var append = "<option value='_NODATA_'>선택하세요</option>";
-			$.each(
-				result,
-				function(index, item) {
-					var id;
-					var name;
-					if(item.usr_id != null){
-						id = item.usr_id;
-						name = item.usr_name;
-					}else if(item.ccd_seq != null){
-						id = item.ccd_seq;
-						name = item.ccd_codename;
-					}else if(item.itm_id != null){
-						id = item.itm_id;
-						name = item.itm_name;
-					}
-					append = append + "<option value='" + id + "'>" + name + "</option>"
-				}
-			)
-			$("#sub_option").html(append);
+			if(response > 0){
+				alert("["+item_name+"] 아이템이 수정되었습니다.");
+				list_item(pagination_index, pagination_cnt, keyword_data);
+			}else{
+				alert("["+item_name+"] 아이템 수정에 실패했습니다.");
+			}
 		},
 		error : function(request, status, error) {
-			mod_user_active_msg("fail",active);
-			console.log("code:" + request.status + "\n" + "message:"
-					+ request.responseText + "\n" + "error:" + error);
+			console.log("code:" + request.status + "\n" + "message:"+ request.responseText + "\n" + "error:" + error);
 		}
 	});
 }
